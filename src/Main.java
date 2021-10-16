99e45a9a54004f683d31ebada72fe2f5f14ee477
1noseA <1noseA2L@gmail.com>
2021-10-16 15:40:55 +0900

    テストデータ

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import com.sample.dao.Author;
 import com.sample.dao.AuthorDao;
 
 public class Main {
@@ -26,7 +27,14 @@ public class Main {
 		AuthorDao dao = session.getMapper(AuthorDao.class);
 		// dao.insert(author);
 		// dao.update(author);
-		dao.delete(1);
+		// dao.delete(1);
+
+		for (int i = 1; i < 10; i++) {
+			Author author = new Author();
+			author.setId(i);
+			author.setName("test" + i);
+			dao.insert(author);
+		}
 
 		session.commit();
 
